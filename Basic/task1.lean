28933f24af80b3f151b70b86e7c7b3dc899d1760
Urh Primožič <urh.primozic@gmail.com>
2024-06-14 23:17:36 +0200

    renamed files

--- Basic/task1.lean
+++ Basic/task1.lean
@@ -1,116 +1,131 @@
--- bijection for task 1
+-- imports
+-- There is a warning "code generator does not support recursor 'Nat.rec' yet, consider using 'match ... with' and/or structural recursion", which we were unable to solve
+import Mathlib.Algebra.BigOperators.Fin
+import Mathlib.Logic.Equiv.Functor
+import Mathlib.Logic.Equiv.Defs
+import Mathlib.Control.Bifunctor
+import Mathlib.Logic.Equiv.Fin
+import Mathlib.Algebra.BigOperators.NatAntidiagonal
+import Mathlib.Data.Finset.NatAntidiagonal
+import Mathlib.Data.Finset.Basic
+import Mathlib.Tactic
+import Mathlib.Data.Nat.Order.Basic
 
-import Mathlib
-import Mathlib.Algebra.BigOperators.Basic
-import Mathlib.Data.Nat.Basic
-import Mathlib.Data.Nat.Choose.Basic
-import Mathlib.Tactic.Basic
-open BigOperators
 open Nat
+open Finset
+open BigOperators
+open Fin
+#align_import algebra.big_operators.fin from "leanprover-community/mathlib"@"cc5dd6244981976cc9da7afc4eee5682b037a013"
 
-#check sub_add
-#check Nat.le_refl
-#check sub_add_cancel
-#check Nat.sub_add_cancel
-#check le_or_lt
-#check Fin.sum_univ_eq_sum_range
-#check Fin.mk
-#check  Equiv.trans
-#check add_comm
-#check sub_lt_sub_right
-#check add_lt_add_right
-#check Fin.sum_univ_castSucc
-#check add_lt_add_iff_right
-#check add_left_neg
-#check Fin.sum_univ_castSucc
+-- IDEA
+--there is finSumFinEquiv, which proves n= 2 for a similar structure (⊕ instead of Σ)
+#check finSumFinEquiv
+/-
+We could prove  (  Σ i : Fin n , Fin ( k i) ) ≃  Fin (∑ i : Fin n, k i) with:
+1. Prove
+ ∑ i : Fin (succ n), k  =  Fin (k 1) ⊕ Fin (k 2) ⊕ ... ⊕ Fin (k n)
+2. Use finSumFinEquiv to prove
+ Fin (k 1) ⊕ Fin (k 2) ⊕ ... ⊕ Fin (k n) = Fin((k 1) + (k 2) + ... + (k n))
+-/
 
+/-
+For step 1, we need to estavlish a connection between Σ and ⊕.
 
--- lema for simplification
-lemma x_minus_plus_strict {x a : ℕ} (h : a < x) : x = (x - a) + a := by
-rw[Nat.sub_add_cancel]
-rw[Nat.lt_iff_le_and_ne] at h
-exact h.left
+First, we need a type representing Fin (k 1) ⊕ Fin (k 2) ⊕ ... ⊕ Fin (k n):
+-/
+def sumOfFin : (n : Nat) → (k : Nat → Nat) → Type
+  | 0, _ => PEmpty -- same as Fin 0
+  | n + 1, k =>  (sumOfFin n (fun i => k (i))) ⊕ (Fin (k n))
 
-lemma x_minus_plus {x a : ℕ} (h : a ≤ x) : x = (x - a) + a := by
-rw[Nat.sub_add_cancel]
-exact h
+-- alternative:
+def RsumOfFin : (n : Nat) → (k : Nat → Nat) → Type
+  | 0, _ => PEmpty
+  | n + 1, k => Sum (Fin (k 0)) (sumOfFin n (fun i => k (Fin.succ i)))
 
-
-example{a b c : Nat} (h1 : a < b) : a + c < b + c :=
-by
-apply add_lt_add_right
-exact h1
-
-example {a b c : Nat} (h1 : a + c < b + c) : a  < b :=
-by
-apply Nat.add_lt_add_iff_right.mp h1
-
-lemma lemma1{a z c : Nat} (h1 : a  < z) : (a + c ) - c < z := by
-simp
-exact h1
+-- EXAMPLE USAGE:
+def kId (n : Nat) (x : Nat) : Nat := x
+#reduce sumOfFin 4 (kId 4)
+#reduce sumOfFin 0 (kId 0)
 
 
+-- USEFUL LEMMAS
 
-lemma extract_full_members {n m: Nat} {k : Fin n → Nat}  {xz : Fin (∑ i : Fin n, k i)}  :
- ∃ j : (Fin (n)) , ∃ m : Fin ( k  j), xz =( ∑ i1 : Fin j.val, k ( Fin.castLE (Fin.is_lt j) i1)) + m := by
-induction n with
-| zero =>
--- zero example is trivial, since Fin0 is empty
---rewrite x to x:fin 0
-  have h_sum : ∑ i : Fin 0, k i = 0 := by simp;
-  rw[h_sum] at xz
-  -- x doesnt exist
-  exfalso
-  exact  Fin.elim0 xz
+-- Equvivalence is a congruence for ⊕:
+lemma sumCong (h: A ≃ B) : A ⊕ C ≃ B ⊕ C := by
+  apply Equiv.sumCongr h
+  apply Equiv.refl C
 
---for n>0, we try if x > k1 + ... + k(n-1). If it is, than m = x - k1 + ... + k(n-1)
--- if its not, than x ∈ Fin(k1 + .. + k(n-1)) and we can usee the induction hypotesis
-| succ nn ih =>
-  let  q := ∑ ii : Fin nn, k ( Fin.castLE (Fin.is_lt nn) ii) ;
-  match le_or_lt q ( xz.val)  with
-  |Or.inl q_leq_x =>
-    --use Fin.ofNat nn as j:
-    use Fin.mk nn (Nat.lt_succ_self nn)
-    -- use x - q as m:
+---------------------
+-- STEP 1 - relation between Σ and ⊕
+---------------------
 
-    have xz_le_sum : xz.val < ( ∑ i : Fin (succ nn), k i )  :=
-        by
-        simp
 
-    have xz_q_q_le_sum_q : (xz.val -q) +q< ( ∑ i : Fin (succ nn), k i ) +q:= by
-      --rw[sub_add_cancel xz.val q]
-      sorry
+--def sigmaNatSucc1 (f : ℕ → Type u) : (Σ n, f n) ≃ Sum (f 0) (Σ n, f (n + 1)) :=
+def sigmaNatSuccUltra {n : Nat} {k : ℕ → ℕ } (f : ℕ → Type u) :
+(  Σ i : Fin (Nat.succ n) , Fin ( k i) )  ≃  (  Σ i : Fin n , Fin ( k i) ) ⊕ Fin (k n) :=
+  ⟨fun x =>
+   --@Sigma.casesOn ℕ f (fun _ => Sum (f 0) (Σn, f (n + 1))) x fun n =>
+    @Sigma.casesOn ℕ (fun m => Fin m)  (fun _ =>((  Σ i : Fin n , Fin ( k i) ) ⊕ Fin (k n)) )x fun n =>
+      @Nat.casesOn (fun i => f i → Sum (f 0) (Σn : ℕ, f (n + 1))) n (fun x : f 0 => Sum.inl x)
+        fun (n : ℕ) (x : f n.succ) => Sum.inr ⟨n, x⟩,
+    Sum.elim (Sigma.mk 0) (Sigma.map Nat.succ fun _ => id), by rintro ⟨n | n, x⟩ <;> rfl, by
+    rintro (x | ⟨n, x⟩) <;> rfl⟩
 
-    -- proof for: xz - q < k(nn)
-    have x_minus_q_le_knn : xz.val - q < k (Fin.mk nn (Nat.lt_succ_self nn)) :=--(Fin.castLE (Nat.lt_succ_self nn) nn) :=
-      by
+
+
+-- First step is to show, that ∑ i : Fin 4, Id  =  Fin 1 ⊕ Fin 2 ⊕ Fin 3
+def sumIsSum {n : Nat} {k : Nat → Nat} :
+   (  Σ i : Fin n , Fin ( k i) ) ≃ sumOfFin n k := by
+    induction n with
+    | zero =>
+            simp
+            reduce
+            apply Equiv.equivOfIsEmpty
+
+    | succ n ih =>
+      -- convert to (***) ⊕ Fin(k n)
+     -- apply Equiv.symm
+      have reduced : sumOfFin (n + 1) k = (  (sumOfFin n k) ⊕ (Fin (k n)) ) := by
+        rfl
+      rw[reduced]
+      #check Equiv.trans
+      apply Equiv.trans
+      apply sigmaNatSuccUltra
+      use (fun n => Fin n)
+      apply sumCong
+      exact ih
+
+
+
+---------------------
+-- STEP 2  - EQUALITY OF ⨁Fin and Fin+
+---------------------
+
+-- Show that  Fin k1 ⊕ Fin k2 ⊕ Fin k3 = Fin(k1 + k2 + k3)
+def finSumnFinEquiv{n : Nat} {k : Nat → Nat} :
+   sumOfFin n k  ≃ Fin (∑ i : Fin n, k i) := by
+   induction n with
+   | zero =>
+             simp
+             reduce
+             apply Equiv.equivOfIsEmpty
+
+   | succ n ih=>
+      have reduced : sumOfFin (n + 1) k = (  (sumOfFin n k) ⊕ (Fin (k n)) ) := by
+        rfl
+      rw[reduced]
+      apply Equiv.symm
+      -- rewrite this into the form for finSumFinEquiv
+      rw[ Fin.sum_univ_castSucc ]
+
+      -- use finSumFinEquiv
+      apply Equiv.trans
+      apply Equiv.symm
+      apply finSumFinEquiv
+
+      apply Equiv.symm
       simp
-      ring_nf
-      sorry
-      --apply add_lt_add_iff_right.mp  xz_q_q_le_sum_q
-
-
-
-
-    use Fin.mk (xz-q) x_minus_q_le_knn
-
-    -- converts ↑{ val := ↑x - q, isLt := x_minus_q_le_knn } to sth nicer
-    ring_nf
-    rw[add_comm]
-    --                 ∑ i1 : Fin j.val, k ( Fin.castLE (Fin.is_lt j) i1))
-    have stupid_lean : ∑ i : Fin nn, k (Fin.castLE (Fin.is_lt nn) i) = ∑ y : Fin nn, k (Fin.castLE (Fin.is_lt nn) y) := by
-      congr
-    rw[stupid_lean]
-    congr
-
-    sorry
-
-  | Or.inr x_le_q =>
-    sorry
-
-
-def fin_to_product : Fin (∑ i : Fin n, k i) → Σ i : Fin n , Fin (k i) :=
-sorry
+      apply sumCong ih
 
 
 
@@ -118,6 +133,10 @@ sorry
 
 
 
-def iso_fin_sum {n : Nat} {k : Fin n → Nat} :
-Fin (∑ i : Fin n, k i) ≅ Σ i : Fin n , Fin (k i) := by
-sorry
+--FINAL EQUALITY:
+
+theorem finSigmanFinEquiv  {n : Nat} {k : Nat → Nat} :
+ (  Σ i : Fin n , Fin ( k i) ) ≃  Fin (∑ i : Fin n, k i) := by
+    apply Equiv.trans
+    apply sumIsSum
+    exact finSumnFinEquiv
